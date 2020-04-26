d73ff27141cd055209def7be00f7dba10b201568
Dayasagara <vgrdaya@gmail.com>
2020-04-26 15:05:35 +0530

    Using form data to upload file

--- handlers/postHandlers/insertFileHandler.go
+++ handlers/postHandlers/insertFileHandler.go
@@ -3,6 +3,7 @@ package postHandlers
 import (
 	"log"
 	"os"
+	"time"
 
 	"github.com/Dayasagara/aws-azure-replication/aws"
 	"github.com/Dayasagara/aws-azure-replication/azure"
@@ -13,31 +14,49 @@ import (
 type PostHandler struct{}
 
 func (p *PostHandler) InsertHandler(ctx echo.Context) error {
-	fileName := "file1"
-	file, err := os.Open(fileName)
-	defer file.Close()
 
+	formFile, err := ctx.FormFile("file")
+	if err != nil {
+		return err
+	}
+	file, err := formFile.Open()
 	if err != nil {
 		return rm.ResponseMapper(400, "File Not Found", ctx)
 	}
+	defer file.Close()
+
+	defer func() {
+		err = os.Remove(formFile.Filename)
+		if err != nil {
+			log.Println("File not deleted")
+		}
+	}()
+
+	dst, err := os.Create(formFile.Filename)
+	if err != nil {
+		return rm.ResponseMapper(400, "File Not Found", ctx)
+	}
+	defer dst.Close()
+
+	fileName := formFile.Filename + " " + time.Now().Format("2006-01-02 15:04:05")
 
 	ctx.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSONCharsetUTF8)
 
 	s3Err := make(chan error)
 	blobErr := make(chan error)
 
-	go aws.InsertIntoS3Bucket(file, s3Err, fileName)
-	go azure.InsertIntoBLOBContainer(file, blobErr, fileName)
+	go aws.InsertIntoS3Bucket(dst, s3Err, fileName)
+	go azure.InsertIntoBLOBContainer(dst, blobErr, fileName)
 
 	s3Response := <-s3Err
 	blobResponse := <-blobErr
 
-	log.Println("s3: ", s3Response)
-	log.Println("Az: ", blobResponse)
-
 	if s3Response == nil && blobResponse == nil {
 		return rm.ResponseMapper(200, "Successfully inserted", ctx)
 	}
+
+	log.Println("s3: ", s3Response)
+	log.Println("Az: ", blobResponse)
 
 	return rm.ResponseMapper(400, "Error in inserting file", ctx)
 
